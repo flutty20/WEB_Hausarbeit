e729f7ffc2a46509e22c24f2a168134a44f9e3c6
Hase Jan <jan.hase@smail.inf.h-brs.de>
2020-08-13 16:59:50 +0200

    Nav leiste links schema fertig

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="body">
-    <header><h1>WEB Hausarbeit</h1></header>
+    <header>
+      <h1>WEB Hausarbeit</h1>
+    </header>
     <nav>ö</nav>
-    <aside>b</aside>
+    <aside>
+      <ul>
+        <li>
+          <a class="active" href="#1">Erfolgsprinzipien des WWW</a>
+        </li>
+        <li>
+          <a href="#2">HTML-Literatur</a>
+        </li>
+        <li>
+          <a href="#3">HTML-Wireframe</a>
+        </li>
+        <li>
+          <a href="#4">Wireframe mit HTML und CSS</a>
+        </li>
+        <li>
+          <a href="#5">Mögliche Klausuraufgaben</a>
+        </li>
+      </ul>
+    </aside>
     <article>c</article>
     <footer>d</footer>
   </div>
@@ -11,77 +31,106 @@
 <script src="https://unpkg.com/vue/dist/vue.js"></script>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
- /*  * {
-            box-sizing: border-box;
-        }*/
+#body {
+  display: grid;
+  grid-template-columns: 1% 18% 1% 79% 1%;
+  grid-template-rows: 1% 8rem 4rem 60rem 1% 8rem 1%;
+  grid-template-areas:
+    ". . . . ."
+    ". header header header ."
+    ". nav nav nav ."
+    ". aside . article . "
+    ". . . . ."
+    ". footer footer footer ."
+    ". . . . . ";
+}
 
-        #body {
-            display: grid;
-            grid-template-columns: 1% 18% 1% 79% 1%;
-            grid-template-rows: 1% 8rem 4rem 60rem 1% 8rem 1%;
-            grid-template-areas:
-            ". . . . ."
-            ". header header header ."
-            ". nav nav nav ."
-            ". aside . article . "
-            ". . . . ."
-            ". footer footer footer ."
-            ". . . . . ";
+header {
+  background-color: red;
+  grid-area: header;
+}
 
-        }
+nav {
+  background-color: orange;
+  grid-area: nav;
+}
 
-        header {
-            background-color: red;
-            grid-area: header;
-        }
-        nav {
-            background-color:orange;
-            grid-area: nav;
-        }
+aside {
+  background-color: limegreen;
+  grid-area: aside;
+}
 
-        aside{
-            background-color: limegreen;
-            grid-area: aside;
-        }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  background-color: #f1f1f1;
+  border: 1px solid #555;
+}
 
-        article {
-            background-color: blue;
-             grid-area: article;
-        }
+li a {
+  display: block;
+  color: #000;
+  padding: 10px 16px;
+  text-decoration: none;
+}
 
-        footer{
-            background-color: magenta;
-            grid-area: footer;
-        }
+li {
+  text-align: center;
+  border-bottom: 1px solid #555;
+}
 
-        /* Responsive layout - when the screen is less than 800px wide, make last column stack on top of the other */
-        @media screen and (max-width: 800px) {
-           #body {
-            display: grid;
-            grid-template-columns: 1% 98% 1%;
-            grid-template-rows: 1% 8rem 4rem 4rem 60rem 1% 8rem 1%;
-            grid-template-areas:
-            ". . . "
-            ". header ."
-            ". nav ."
-            ". aside . "
-            ".article ."
-            ". . ."
-            ". footer ."
-            ". . .";
-        }
-        }
+li:last-child {
+  border-bottom: none;
+}
 
+li a.active {
+  background-color: darkgreen;
+  color: white;
+}
+
+li a:hover:not(.active) {
+  background-color: green;
+  color: white;
+}
+
+article {
+  background-color: blue;
+  grid-area: article;
+}
+
+footer {
+  background-color: magenta;
+  grid-area: footer;
+}
+
+/* Responsive layout - when the screen is less than 800px wide, make last column stack on top of the other */
+@media screen and (max-width: 800px) {
+  #body {
+    display: grid;
+    grid-template-columns: 1% 98% 1%;
+    grid-template-rows: 1% 8rem 4rem 4rem 60rem 1% 8rem 1%;
+    grid-template-areas:
+      ". . . "
+      ". header ."
+      ". nav ."
+      ". aside . "
+      ".article ."
+      ". . ."
+      ". footer ."
+      ". . .";
+  }
+}
 </style>
