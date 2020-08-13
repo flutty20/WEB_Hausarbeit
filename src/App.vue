2f606aa9006a15912968271aead32f2c30070439
Hase Jan <jan.hase@smail.inf.h-brs.de>
2020-08-13 15:17:54 +0200

    Grid eingerichtet

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="body">
+    <header>a</header>
+    <nav>ö</nav>
+    <aside>b</aside>
+    <article>c</article>
+    <footer>d</footer>
   </div>
 </template>
+
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -14,15 +19,50 @@ export default {
     HelloWorld
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ /*  * {
+            box-sizing: border-box;
+        }*/
+
+        #body {
+            display: grid;
+            grid-template-columns: 1% 18% 1% 79% 1%;
+            grid-template-rows: 2% 8rem 4rem 60rem 2% 8rem 2%;
+            grid-template-areas:
+            ". . . . ."
+            ". header header header ."
+            ". nav nav nav ."
+            ". aside . article . "
+            ". . . . ."
+            ". footer footer footer ."
+            ". . . . . ";
+
+        }
+
+        header {
+            background-color: red;
+            grid-area: header;
+        }
+        nav {
+            background-color:orange;
+            grid-area: nav;
+        }
+
+        aside{
+            background-color: limegreen;
+            grid-area: aside;
+        }
+
+        article {
+            background-color: blue;
+             grid-area: article;
+        }
+
+        footer{
+            background-color: magenta;
+            grid-area: footer;
+        }
 </style>
